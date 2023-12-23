237890e1ae78f7966abeaf9e9d95e6e8a3792ba2
hitesh patel <hitesh@pixelhexdigital.com>
2023-12-23 16:43:03 +0530

    utils setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 8000;
+    app.on("error", (error) => {
+      console.error("ERR", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGODB connection failed !!!", err);
+  });
